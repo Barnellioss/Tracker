e0ca89c617653ca594bb0eccb9e60a19aed37fe9
unknown <wotpovorot@gmail.com>
2021-03-09 19:25:25 +0200

    bugsFixed

---
+++ src/redux/types.js
@@ -0,0 +1,14 @@
+export const SHOW_ALERT = "SHOW_ALERT"
+export const HIDE_ALERT = "HIDE_ALERT"
+export const ADD_NOTE = "ADD_NOTE"
+export const FETCH_NOTES = "FETCH_NOTES"
+export const REMOVE_NOTE = "REMOVE_NOTE"
+export const REACT_APP_DB_URL = 'https://trackerbase-9239b-default-rtdb.europe-west1.firebasedatabase.app/'
+export const DONE_TASK = 'DONE_TASK'
+export const STOP_TIMER = 'STOP_TIMER'
+export const RESUME_TIMER = 'RESUME_TIMER'
+export const RESET_TIMER = 'RESET_TIMER'
+
+
+
+
